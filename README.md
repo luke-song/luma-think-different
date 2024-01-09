a3d0c8281d522e68462a2df5ce6f2d23f4288e3f
Luke Song <hyeonminsong7@gmail.com>
2024-01-09 03:08:19 -0800

    :recycle: clean up vite-template,:lipstick: Added Page Layout, :dizzy: Added fadeIn,fadeOut,fadeInOut Animation and Typing animation, :sparkles: Added 3d object, light and camera,:sparkles: Added react-intersection-observer for scrolling, Story Added

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
-# React + Vite
+<p align="middle" >
+  <img width="300" alt="grape_8" src="./src/assets/README.png">
+</p>
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+<p align="middle">
+   <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white"> 
+   <img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white"> 
+   <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</p>
 
-Currently, two official plugins are available:
+# Story
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Think Different Page is about a boy who dropped out. Embark on an interactive journey that traces the path of a college dropout who transformed the world with his unique vision. The story begins with the pivotal moment of leaving formal education, symbolized by a fading phrase, “Once upon a time, one college student dropped out.” This marks the start of an unconventional path towards groundbreaking innovations.
+
+Interactivity:
+The webpage features a section titled "MerryGoAround," showcasing 3D animations created with React Spring, illustrating the dynamic and ever-evolving nature of innovation. Users are then led to a segment highlighting the dropout's encounter with calligraphy, emphasizing the fusion of art and technology in his later creations.
+
+Animations and Visuals:
+The page includes an engaging "Hello World!" section with Leo-font-animation, symbolizing the first steps in programming and technology. An optional interactive 3D image of a sliding Macintosh computer is presented, offering a zoom-in perspective and an interactive monitor, underscoring the revolutionary impact of personal computing.
+
+Tribute Section:
+A special tribute to Steve Jobs is envisaged with an optional interactive portrait.
+This section includes a random Steve Jobs quote generator, adding an element of surprise and inspiration for the visitors.
+The famous quote,
+“You can’t connect the dots looking forward; you can only connect them looking backwards..."
+is prominently featured, encapsulating the essence of his philosophy.
+
+Educational Element:
+The webpage plans to have an optional learning section about serif fonts and handwriting, tying back to the influence of calligraphy on Steve Jobs' approach to design and aesthetics.
+
+Technical Showcase:
+'@react-three/fiber' and '@react-spring/three'
+
+## :runner: Installation
+
+1. Clone Repository
+
+   ```bash
+   git clone "https://github.com/luke-song/luma-think-different"
+   ```
+
+2. Install modules
+
+   ```bash
+   npm install
+   ```
+
+3. Run App
+   ```
+   npm run dev
+   ```
+
+## 👨‍👩‍👦‍👦 Contribution
+
+| name | Role               |
+| ---- | ------------------ | ------------------------------------------ |
+| Luke | Front-End Engineer | [@luke-song](https://github.com/luke-song) |
+
+## 🗂 File Structure
+
+```
+📦Root
+ ┣ 📄.gitignore
+ ┣ 📄package.json
+ ┣ 📄README.md
+ ┣ 📄index.html
+ ┣ 📄vite.config.js
+ ┣ 📄.eslintrc.cjs
+ ┗ 📂src
+   ┣ 📄App.css
+   ┣ 📄App.jsx
+   ┣ 📄index.css
+   ┣ 📄main.jsx
+   ┗ 📂assets
+     ┗ 🖼️README.png
+
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import TypeIt from "typeit-react";
+import { useInView } from "react-intersection-observer";
+import React, { useRef, useState } from "react"; //useReducer, useMemo
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Fisheye,
+  CameraControls,
+  PerspectiveCamera,
+  Environment,
+  useGLTF,
+} from "@react-three/drei";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { ref: sectionRef, inView: sectionInView } = useInView();
+  const { ref: section1Ref, inView: section1InView } = useInView();
+  const { ref: section2Ref, inView: section2InView } = useInView();
+  const { ref: section3Ref, inView: section3InView } = useInView();
+  const { ref: section4Ref, inView: section4InView } = useInView();
+  const { ref: section5Ref, inView: section5InView } = useInView();
+  const { ref: section6Ref, inView: section6InView } = useInView();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="container">
+      <section ref={sectionRef} className="">
+        <div className="fade-in-out">
+          Once upon a time, one college student dropped out.
+        </div>
+        <button
+          className="scroll-down-button"
+          onClick={() =>
+            section1Ref.current.scrollIntoView({ behavior: "smooth" })
+          }
+        />
+      </section>
+
+      <section ref={section1Ref}>
+        <Canvas flat>
+          {/* <Fisheye zoom={0}> */}
+          {/* <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.6} /> */}
+          <ambientLight intensity={Math.PI / 2} />
+          <directionalLight position={[0, 0, 5]} color="white" />
+          <pointLight position={[10, 10, 10]} />
+          <RotatingBox />
+          <Environment preset="city" background blur={1} />
+          <PerspectiveCamera makeDefault position={[0, 0, 18.5]} />
+          {/* </Fisheye> */}
+        </Canvas>
+      </section>
+
+      <section ref={section2Ref}>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {section2InView && (
+            <TypeIt options={{ speed: 50 }}>
+              Dropping in, he met calligraphy.
+            </TypeIt>
+          )}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+
+      <section ref={section3Ref}>
+        <p>
+          {section3InView && (
+            <TypeIt options={{ speed: 50 }}>“Hello World!”</TypeIt>
+          )}
+        </p>
+      </section>
+
+      <section ref={section4Ref}>
+        <p>
+          {section4InView && (
+            <TypeIt options={{ speed: 50 }}>
+              The mac would have never had multiple typefaces or proportionally
+              spaced fonts.
+            </TypeIt>
+          )}
+        </p>
+      </section>
+
+      <section ref={section4Ref}>
+        <p>
+          {section4InView && (
+            <TypeIt options={{ speed: 50 }}>
+              And Since the Windows just copied the Mac, it's likely that no
+              personal computer would have them.
+            </TypeIt>
+          )}
+        </p>
+      </section>
+
+      <section ref={section5Ref}>
+        <p>
+          {section5InView && (
+            <TypeIt options={{ speed: 50 }}>
+              You can’t connect the dots looking forward; you can only connect
+              them looking backwards. So you have to trust that the dots will
+              somehow connect in your future. You have to trust in something —
+              your gut, destiny, life, karma, whatever.
+            </TypeIt>
+          )}
+        </p>
+      </section>
+
+      <section ref={section6Ref}>
+        {section6InView && <p>Sorry. Will be Ready Soon</p>}
+      </section>
+    </div>
+  );
 }
 
-export default App
+function RotatingBox() {
+  const [active, setActive] = useState(false);
+
+  // const { nodes, materials } = useGLTF("/model.glb");
+
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  return (
+    <mesh ref={ref} onClick={() => setActive(!active)} scale={active ? 1.5 : 1}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshNormalMaterial color="white" />
+    </mesh>
+  );
+}
